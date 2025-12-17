4caaa62a6b161e9aa23c4f888b230a66288ca144
Jishnu <jishnupatel1@gmail.com>
2025-12-16 19:45:13 -0600

    updated example_tier_calculator

--- utils/example_tier_calculator.py
+++ utils/example_tier_calculator.py
@@ -2,6 +2,8 @@
 # This tier calculator assigns scores based on overall.
 
 def calculate_tier(company_data):
+    if (company_data["overall"] == ''):
+        return 'N/A'
     if float(company_data["overall"]) < 3.4:
         return 'C'
     elif float(company_data["overall"]) < 3.8:
